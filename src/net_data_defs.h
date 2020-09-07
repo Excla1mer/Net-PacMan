7d8d7f011c6fd6b17e99901ed0796162b51c987f
bigger777 <biggerlilolol@gmail.com>
2020-09-08 01:59:54 +0700

    Add defines for net_data

---
+++ src/net_data_defs.h
@@ -0,0 +1,13 @@
+#ifndef NET_DATA_DEFS_H
+#define NET_DATA_DEFS_H
+
+/* Формат данных: data[TYPE, DATA, DATA] */
+
+/* TYPE */
+#define ID_PORT 0
+#define READY 1
+#define START 2
+#define CL_READY 3
+#define CL_CONNECT 4
+
+#endif // NET_DATA_DEFS_H
